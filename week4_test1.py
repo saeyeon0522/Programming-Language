a6aa0a1807f3e79e211bdc7ee4a9ed91c05613f5
saeyeon0522 <sy01lim@naver.com>
2023-01-25 19:11:24 +0900

    2023-01-25 19:10

--- week4_test1.py
+++
@@ -1,4 +0,0 @@
-num = int(input("정수를 입력하시오:"))
-print(f"{num}의 2진수 값={bin(num)}")
-print(f"{num}의 8진수 값={oct(num)}")
-print(f"{num}의 16진수 값={hex(num)}")          
--- week4_test2.py
+++
@@ -1,6 +0,0 @@
-char = input("알파벳을 입력하시오:")
-sign = 1 if 'A' <= char <= 'Z' else 0
-if sign == 1:
-    print(f"당신이 입력한 문자 {char}은 대문자입니다.")
-else:
-    print(f"당신이 입력한 문자 {char}은 소문자입니다.")
